0bdf9a1126490221c714ad1031b75714cf2f870a
luthfanubai <luthfan.edogawa@gmail.com>
2016-11-24 20:20:52 +0700

    spawn controller added, restructure folder

---
+++ Assets/Scripts/SpawnSite.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnSite : MonoBehaviour
+{
+    static PlayerController player;
+    GameObject monsterPrefab;
+
+    Vector3 spawnedPosition;
+
+    float enemyRadar = 30f;
+    float respawnTime = 3f;
+
+    public bool playerDetected
+    {
+        get
+        {
+            return (player.FocusObject.position - transform.position).magnitude <= enemyRadar;
+        }
+    }
+
+    public void Activate(GameObject monsterPrefab)
+    {
+        if (this.monsterPrefab != null)
+            return;
+        
+        player = GameObject.FindWithTag ("Player").GetComponent<PlayerController> ();
+        this.monsterPrefab = monsterPrefab;
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        GameObject monsterClone;
+        while (true)
+        {
+            if (playerDetected)
+            {
+                spawnedPosition = transform.position;
+                monsterClone = (GameObject)Instantiate(monsterPrefab, spawnedPosition, Quaternion.identity);
+                monsterClone.SetActive(true);
+
+                while (monsterClone != null)
+                {
+                    if ((player.FocusObject.position - monsterClone.transform.position).magnitude > enemyRadar)
+                    {
+                        monsterClone.SetActive(false);
+                        monsterClone.transform.position = transform.position;
+                    }
+                    else if (playerDetected)
+                        monsterClone.SetActive(true);
+                    
+                    yield return null;
+                }
+                yield return new WaitForSeconds(respawnTime);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] monsterPrefabs;
+    SpawnSite[] spawnSites;
+
+	// Use this for initialization
+	void Start () {
+        
+        int number = monsterPrefabs.Length;
+        spawnSites = new SpawnSite[number];
+        for (int i = 0; i < number; i++)
+        {
+            Vector3 randomPosition = transform.position + Random.insideUnitSphere * 20;
+            randomPosition.y = transform.position.y;
+
+            spawnSites[i] = new GameObject("Spawn Site " + (i + 1)).AddComponent<SpawnSite>();
+            spawnSites[i].transform.SetParent(transform);
+            spawnSites[i].transform.position = randomPosition;
+            spawnSites[i].Activate(monsterPrefabs[i]);
+        }
+	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+}
